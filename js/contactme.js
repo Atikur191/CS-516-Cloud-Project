e69c68791d415a8574013a47eae6562260b4b341
atikur191 <atikur.rahman1562@gmail.com>
2023-10-22 22:38:29 -0500

    blog commit

--- js/contactme.js
+++ js/contactme.js
@@ -1,5 +1,3 @@
-
-
 console.log("Called");
 
 // axios.get('https://reqres.in/api/users?page=2')
@@ -7,52 +5,49 @@ console.log("Called");
 // .catch((err)=> console.log(err));
 
 
-
 // $("#contactme-form").on("submit", function (event) {
 //     console.log("Submit button click");
-   
+
 // });
 
 function objectifyForm(formArray) {
     //serialize data function
     var returnArray = {};
-    for (var i = 0; i < formArray.length; i++){
+    for (var i = 0; i < formArray.length; i++) {
         returnArray[formArray[i]['name']] = formArray[i]['value'];
     }
     return returnArray;
 }
 
 $("#contactme-form").on("submit", function (event) {
- console.log("Javascript called");
+    console.log("Javascript called");
 
- var formData = $("#contactme-form").serializeArray();
- var formSubmit = objectifyForm(formData);
- var simplified = JSON.stringify(formSubmit);
- console.log(simplified);
+    var formData = $("#contactme-form").serializeArray();
+    var formSubmit = objectifyForm(formData);
+    console.log(formSubmit);
 
- axios.post('https://glrncltyferm23mh5hw77qc4c40pdzyb.lambda-url.us-east-1.on.aws/',
-{
-    headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}
-    ,
-    //"input": "{\"sendername\": \"Sujit\",\"emailaddress\": [\"atikur.rahman1562@gmail.com\"],\"telephone\": \"phone\",\"sendersubject\": \"subject\",\"sendermessage\":\"comment\"}",
-    "stateMachineArn": "arn:aws:states:us-east-1:717879216935:stateMachine:SMForEmailSend",
-    "input": simplified
-})
-  .then((response) => {
-    console.log("atikur - OK");
-    //alert("Your Requested Submitted Successfully ");
-    console.log(response);
+    axios.post('https://1stxc604z2.execute-api.us-east-1.amazonaws.com/v1/project/', formSubmit, {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Methods': '*'
+        },
+    })
+        .then((response) => {
+            console.log("atikur - OK");
+            //alert("Your Requested Submitted Successfully ");
+            console.log(response);
 
-    document.getElementById("submit_response").innerHTML = "Request has been successfully saved";
-    document.getElementById("contactme-form").reset();
-    
-  })
-  .catch( (error) => {
-    console.log("atikur - error");
-    //alert("Ops !!! Failed to Submit. Retry again...");
-    document.getElementById("submit_response").innerHTML = "Unknown Error Happened. Please try again";
-    console.log(error);
-  });
-  return false;
+            document.getElementById("submit_response").innerHTML = "Request has been successfully saved";
+            document.getElementById("contactme-form").reset();
+
+        })
+        .catch((error) => {
+            console.log("atikur - error");
+            //alert("Ops !!! Failed to Submit. Retry again...");
+            document.getElementById("submit_response").innerHTML = "Unknown Error Happened. Please try again";
+            console.log(error);
+        });
+    return false;
 });
 
